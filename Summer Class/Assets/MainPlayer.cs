820ec6e7444f761a5a981b060380a5e12ad07540
Megyn <10996410@uvu.edu>
2022-06-21 19:12:44 -0600

    Variables and the Inspector Tweaks

--- Summer Class/Assets/MainPlayer.cs
+++ Summer Class/Assets/MainPlayer.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class MainPlayer : MonoBehaviour
 {
+    public string myName;
     // Use this for initialization
     void Start()
     {
-        Debug.Log("I am alive!")
+        Debug.Log("I am alive and my name is" + myName);
     }
 }
