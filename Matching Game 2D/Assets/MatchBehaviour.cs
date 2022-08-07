47cd9051ec528bac84c12b4f9e9482d7d8839ae6
Megyn <10996410@uvu.edu>
2022-08-06 19:14:50 -0600

    Tweaks for Challenge 7b

--- Matching Game 2D/Assets/MatchBehaviour.cs
+++ Matching Game 2D/Assets/MatchBehaviour.cs
@@ -1,9 +1,11 @@
 
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehaviour : MonoBehaviour
 {
    public ID idObj;
+   public UnityEvent matchEvent, noMatchEvent;
    private void OnTriggerEnter(Collider other)
    {
       var tempObj = other.GetComponent<IDContainerBehaviour>();
@@ -13,7 +15,11 @@ public class MatchBehaviour : MonoBehaviour
       var otherID = tempObj.idObj;
       if (otherID == idObj)
       {
-         Debug.Log("Matched");
+         matchEvent.Invoke();
+      }
+      else
+      {
+         noMatchEvent.Invoke();
       }
    }
 }
